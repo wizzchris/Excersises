95f3994c3d9310158ccf0231a8f2a78b6a088cdb
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-04 20:38:42 +0000

    Added the inputs

---
+++ Excersise2.py
@@ -0,0 +1,7 @@
+name = 'Lana'
+last_name = input('What is your last name?    ')
+species = input('What is your species?        ')
+eye_colour = input('What is your eye colour?       ')
+hair_colour = input('What is your hair colour?      ')
+
+ 
