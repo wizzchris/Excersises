c6d231503a8e102843d0cc838b7e862474ccd033
Christopher Rogers <CRogers@spartaglobal.com>
2020-02-04 20:43:20 +0000

    Added the convo

--- Excersise2.py
+++ Excersise2.py
@@ -4,4 +4,4 @@ species = input('What is your species?        ')
 eye_colour = input('What is your eye colour?       ')
 hair_colour = input('What is your hair colour?      ')
 
- 
+print('Hello' + ' ' + name + ' ' + last_name + ' ' + 'you are a' + ' ' + species + '. ' + 'And you have' + ' ' + eye_colour + ' ' + 'eyes' + ' ' + 'and your colour hair is' + ' ' + hair_colour)
